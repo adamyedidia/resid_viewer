b3d4d249bf9157fe17db7b1a1a4372a3b1725b13
Adam Yedidia <adamyedidia@gmail.com>
2023-07-07 19:01:31 -0400

    Add imports to ipython

--- ipython.py
+++ ipython.py
@@ -1,12 +1,23 @@
 from IPython import embed
 from database import SessionLocal, engine
 from vector import Vector
+from resid import Resid
+from model import Model
+from user import User
+from direction import Direction
 
 def main():
     sess = SessionLocal()
 
     # add objects that you want to use in the shell to this dictionary
-    user_ns = {"sess": sess, "Vector": Vector}
+    user_ns = {
+        "sess": sess, 
+        "Vector": Vector,
+        "Resid": Resid,
+        "Model": Model,
+        "User": User,
+        "Direction": Direction,
+    }
 
     print("Starting IPython shell with session and Vector imported.")
     embed(user_ns=user_ns)
