6ce61ca3ae7d9a4f8411a902795310073e3345a2
Adam Yedidia <adamyedidia@gmail.com>
2023-07-06 22:26:05 -0400

    sess instead of session

--- ipython.py
+++ ipython.py
@@ -3,15 +3,15 @@ from database import SessionLocal, engine
 from vector import Vector
 
 def main():
-    session = SessionLocal()
+    sess = SessionLocal()
 
     # add objects that you want to use in the shell to this dictionary
-    user_ns = {"session": session, "Vector": Vector}
+    user_ns = {"session": sess, "Vector": Vector}
 
     print("Starting IPython shell with session and Vector imported.")
     embed(user_ns=user_ns)
 
-    session.close()
+    sess.close()
 
 if __name__ == "__main__":
     main()
