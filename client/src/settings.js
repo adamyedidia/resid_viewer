34560119cf20960f05bfb7583b6a04b25bf3d269
Adam Yedidia <adamyedidia@gmail.com>
2024-04-04 14:37:55 -0400

    Fix backend URL

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_URL || 'http://ec2-34-192-101-140.compute-1.amazonaws.com:5013';
+export const API_URL = process.env.REACT_APP_URL || 'http://ec2-100-26-158-100.compute-1.amazonaws.com:5013';
